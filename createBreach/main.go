c72ef3af50c221136d544fb13b64e79ca897e365
Josh Choo <dev@joshuous.com>
2021-06-21 15:44:51 +0800

    Connect to DynamoDB Breaches table and list tables from createBreach function

--- createBreach/main.go
+++ createBreach/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -14,9 +17,27 @@ import (
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
+var sess = session.Must(session.NewSessionWithOptions(session.Options{
+	SharedConfigState: session.SharedConfigEnable,
+}))
+
+// Create the DynamoDB client
+var svc = dynamodb.New(sess)
+
 func Handler(ctx context.Context) (Response, error) {
+	input := &dynamodb.ListTablesInput{}
+	result, err := svc.ListTables(input)
+	if err != nil {
+		return Response{StatusCode: 500, Body: "Failed to list DynamoDB tables"}, err
+	}
+
+	tableNames := []string{}
+	for _, name := range result.TableNames {
+		tableNames = append(tableNames, *name)
+	}
+
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "Created new breach successfully.",
+		"message": strings.Join(tableNames, "\n"),
 	})
 	if err != nil {
 		return Response{StatusCode: 400}, err
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.6.0
+	github.com/aws/aws-sdk-go v1.38.61 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 )
