aac0cedf97e374809c6ec4d552736cf5b15eddc8
Josh Choo <dev@joshuous.com>
2021-06-22 09:32:26 +0800

    Add findAccount API

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ build:
 	export GO111MODULE=on
 	env GOOS=linux CGO_ENABLED=0 go build -ldflags="-s -w" -o bin/addAccountsToBreach addAccountsToBreach/main.go
 	env GOOS=linux CGO_ENABLED=0 go build -ldflags="-s -w" -o bin/createBreach createBreach/main.go
+	env GOOS=linux CGO_ENABLED=0 go build -ldflags="-s -w" -o bin/findAccount findAccount/main.go
 
 clean:
 	rm -rf ./bin ./vendor
---
+++ findAccount/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/joshuous/haveibeenbreached"
+)
+
+type Response events.APIGatewayProxyResponse
+
+type FindAccountEvent struct {
+	PathParameters struct {
+		Username string
+	}
+}
+
+var sess = session.Must(session.NewSessionWithOptions(session.Options{
+	SharedConfigState: session.SharedConfigEnable,
+}))
+var svc = dynamodb.New(sess)
+var repo = haveibeenbreached.NewRepo(svc)
+var findAccountHandler = makeFindAccountHandler(repo)
+
+func main() {
+	lambda.Start(findAccountHandler)
+}
+
+func makeFindAccountHandler(repo haveibeenbreached.Repo) func(ctx context.Context, event FindAccountEvent) (Response, error) {
+	return func(ctx context.Context, event FindAccountEvent) (Response, error) {
+		rawUsername := event.PathParameters.Username
+
+		username, err := haveibeenbreached.NewEmail(rawUsername)
+		if err != nil {
+			return Response{StatusCode: 400, Body: fmt.Sprintf("Invalid username: %s", rawUsername)}, err
+		}
+
+		account, err := repo.GetAccount(username)
+		if err != nil {
+			return Response{StatusCode: 400, Body: fmt.Sprintf("Error finding account: %s", err)}, err
+		}
+		if account == nil {
+			return Response{StatusCode: 404, Body: "No such account."}, err
+		}
+
+		body, err := json.Marshal(map[string]interface{}{
+			"message": "Found breaches for account.",
+			"data": map[string]interface{}{
+				"Breaches": account.Breaches,
+			},
+		})
+		if err != nil {
+			return Response{StatusCode: 400}, err
+		}
+
+		resp := Response{
+			StatusCode:      200,
+			IsBase64Encoded: false,
+			Body:            string(body),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+
+		return resp, nil
+	}
+}
